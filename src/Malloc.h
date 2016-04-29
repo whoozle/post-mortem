bd5fe7340a740950b49205130cce8918af028d12
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-04-29 16:19:14 +0100

    use glibc prototypes if available

--- src/Malloc.h
+++ src/Malloc.h
@@ -4,6 +4,16 @@
 #include <dlfcn.h>
 #include <malloc.h>
 
+#ifdef __GNU_LIBRARY__
+extern "C"
+{
+	void *__libc_malloc(size_t size);
+	void __libc_free(void *ptr);
+	void *__libc_calloc(size_t nmemb, size_t size);
+	void *__libc_realloc(void *ptr, size_t size);
+}
+#endif
+
 namespace Malloc
 {
 	void * (*_malloc)(size_t size) = 0;
@@ -16,11 +26,17 @@ namespace Malloc
 		if (_malloc)
 			return;
 
+#ifdef __GNU_LIBRARY__
+		_malloc = &__libc_malloc;
+		_free = &__libc_free;
+		_realloc = &__libc_realloc;
+		_calloc = &__libc_calloc;
+#else
 		_malloc	= reinterpret_cast<decltype(_malloc)>(dlsym(RTLD_NEXT, "malloc"));
 		_free	= reinterpret_cast<decltype(_free)>(dlsym(RTLD_NEXT, "free"));
 		_realloc= reinterpret_cast<decltype(_realloc)>(dlsym(RTLD_NEXT, "realloc"));
 		_calloc	= reinterpret_cast<decltype(_calloc)>(dlsym(RTLD_NEXT, "calloc"));
-
+#endif
 		if (!_malloc || !_free || !_calloc || !_realloc)
 		{
 			static const char *error = "dlsym(RTLD_NEXT) failed\n";
