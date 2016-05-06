38df425658f209c0c74c531815884afd3c16befd
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-05-06 18:52:36 +0100

    call malloc/free from special cases of realloc

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,13 @@ extern "C"
 	__attribute__ ((visibility ("default")))
 	void *realloc(void *ptr, size_t size)
 	{
+		if (ptr == NULL)
+			return malloc(size);
+		if (size == 0)
+		{
+			free(ptr);
+			return NULL;
+		}
 		void *p = Malloc::Realloc(ptr, size);
 		if (p)
 		{
