91125b7fdde2a49bbe9290460682e10e5ac3acd5
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-05-06 13:29:47 +0100

    marked everything as extern "C"

--- src/main.cpp
+++ src/main.cpp
@@ -3,45 +3,70 @@
 #include "Malloc.h"
 #include <new>
 
-__attribute__ ((visibility ("default")))
-void *malloc(size_t size)
-{
-	void *p = Malloc::Alloc(size);
-	Monitor::Alloc(p, size);
-	return p;
-}
 
-__attribute__ ((visibility ("default")))
-void free(void *ptr)
+extern "C"
 {
-	Monitor::Free(ptr);
-	Malloc::Free(ptr);
-}
 
-__attribute__ ((visibility ("default")))
-void *calloc(size_t nmemb, size_t size)
-{
-	void *p = Malloc::Calloc(nmemb, size);
-	Monitor::Alloc(p, nmemb * size);
-	return p;
-}
+    __attribute__ ((visibility ("default")))
+    void *malloc(size_t size)
+    {
+        void *p = Malloc::Alloc(size);
+        Monitor::Alloc(p, size);
+        return p;
+    }
 
-__attribute__ ((visibility ("default")))
-void *realloc(void *ptr, size_t size)
-{
-	void *p = Malloc::Realloc(ptr, size);
-	if (p)
-	{
-		Monitor::Free(ptr);
-		Monitor::Alloc(p, size);
-	}
-	else if (size == 0)
-	{
-		Monitor::Free(ptr);
-	}
-	return p;
-}
+    __attribute__ ((visibility ("default")))
+    void free(void *ptr)
+    {
+        Monitor::Free(ptr);
+        Malloc::Free(ptr);
+    }
 
+    __attribute__ ((visibility ("default")))
+    void *calloc(size_t nmemb, size_t size)
+    {
+        void *p = Malloc::Calloc(nmemb, size);
+        Monitor::Alloc(p, nmemb * size);
+        return p;
+    }
+
+    __attribute__ ((visibility ("default")))
+    void *realloc(void *ptr, size_t size)
+    {
+        void *p = Malloc::Realloc(ptr, size);
+        if (p)
+        {
+            Monitor::Free(ptr);
+            Monitor::Alloc(p, size);
+        }
+        else if (size == 0)
+        {
+            Monitor::Free(ptr);
+        }
+        return p;
+    }
+
+    __attribute__ ((visibility ("default")))
+    int posix_memalign(void **memptr, size_t alignment, size_t size)
+    { return -1; }
+
+    __attribute__ ((visibility ("default")))
+    void *aligned_alloc(size_t alignment, size_t size)
+    { return NULL; }
+
+    __attribute__ ((visibility ("default")))
+    void *valloc(size_t size)
+    { return NULL; }
+
+    __attribute__ ((visibility ("default")))
+    void *memalign(size_t alignment, size_t size)
+    { return NULL; }
+
+    __attribute__ ((visibility ("default")))
+    void *pvalloc(size_t size)
+    { return NULL; }
+
+}
 
 __attribute__ ((visibility ("default")))
 void* operator new(size_t size)
@@ -90,24 +115,3 @@ void operator delete[] (void *p, const std::nothrow_t& ) throw()
 {
 	return free(p);
 }
-
-
-__attribute__ ((visibility ("default")))
-int posix_memalign(void **memptr, size_t alignment, size_t size)
-{ return -1; }
-
-__attribute__ ((visibility ("default")))
-void *aligned_alloc(size_t alignment, size_t size)
-{ return NULL; }
-
-__attribute__ ((visibility ("default")))
-void *valloc(size_t size)
-{ return NULL; }
-
-__attribute__ ((visibility ("default")))
-void *memalign(size_t alignment, size_t size)
-{ return NULL; }
-
-__attribute__ ((visibility ("default")))
-void *pvalloc(size_t size)
-{ return NULL; }
