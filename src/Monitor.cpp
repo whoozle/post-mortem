1040c1947b01b25dd859108c9e47da63d8f0cdb0
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-05-06 18:51:50 +0100

    converted back to tabs again

--- src/Monitor.cpp
+++ src/Monitor.cpp
@@ -12,46 +12,46 @@ bool __thread Monitor::_bypass = false;
 
 int Monitor::GetFD()
 {
-    if (_fd < 0)
-    {
-        char exenamebuf[BUFSIZ];
-        readlink("/proc/self/exe", exenamebuf, BUFSIZ);
-        char *exename = strrchr(exenamebuf, '/');
-        if (exename == NULL)
-            exename = exenamebuf;
-        else
-            ++exename;
-        char filename[BUFSIZ];
-        snprintf(filename, sizeof(filename), "post-mortem.%s.%d.log", exename, getpid());
-        _fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-        if (_fd == -1)
-        {
-            static const char *error = "open: cannot open post-mortem.log\n";
-            write(STDOUT_FILENO, error, strlen(error));
-            abort();
-        }
-        unsigned version = 0;
-        write(_fd, &version, sizeof(version));
-    }
-    return _fd;
+	if (_fd < 0)
+	{
+		char exenamebuf[BUFSIZ];
+		readlink("/proc/self/exe", exenamebuf, BUFSIZ);
+		char *exename = strrchr(exenamebuf, '/');
+		if (exename == NULL)
+			exename = exenamebuf;
+		else
+			++exename;
+		char filename[BUFSIZ];
+		snprintf(filename, sizeof(filename), "post-mortem.%s.%d.log", exename, getpid());
+		_fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+		if (_fd == -1)
+		{
+			static const char *error = "open: cannot open post-mortem.log\n";
+			write(STDOUT_FILENO, error, strlen(error));
+			abort();
+		}
+		unsigned version = 0;
+		write(_fd, &version, sizeof(version));
+	}
+	return _fd;
 }
 
 void Monitor::Alloc(void *p, size_t size)
 {
-    if (!p || _bypass)
-        return;
+	if (!p || _bypass)
+		return;
 
-    int fd = GetFD();
-    Record record(RecordType::Alloc, p, size);
-    write(fd, static_cast<void *>(&record), sizeof(record));
+	int fd = GetFD();
+	Record record(RecordType::Alloc, p, size);
+	write(fd, static_cast<void *>(&record), sizeof(record));
 }
 
 void Monitor::Free(void *p)
 {
-    if (!p || _bypass)
-        return;
+	if (!p || _bypass)
+		return;
 
-    int fd = GetFD();
-    Record record(RecordType::Free, p);
-    write(fd, static_cast<void *>(&record), sizeof(record));
+	int fd = GetFD();
+	Record record(RecordType::Free, p);
+	write(fd, static_cast<void *>(&record), sizeof(record));
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,64 +7,64 @@
 extern "C"
 {
 
-    __attribute__ ((visibility ("default")))
-    void *malloc(size_t size)
-    {
-        void *p = Malloc::Alloc(size);
-        Monitor::Alloc(p, size);
-        return p;
-    }
+	__attribute__ ((visibility ("default")))
+	void *malloc(size_t size)
+	{
+		void *p = Malloc::Alloc(size);
+		Monitor::Alloc(p, size);
+		return p;
+	}
 
-    __attribute__ ((visibility ("default")))
-    void free(void *ptr)
-    {
-        Monitor::Free(ptr);
-        Malloc::Free(ptr);
-    }
+	__attribute__ ((visibility ("default")))
+	void free(void *ptr)
+	{
+		Monitor::Free(ptr);
+		Malloc::Free(ptr);
+	}
 
-    __attribute__ ((visibility ("default")))
-    void *calloc(size_t nmemb, size_t size)
-    {
-        void *p = Malloc::Calloc(nmemb, size);
-        Monitor::Alloc(p, nmemb * size);
-        return p;
-    }
+	__attribute__ ((visibility ("default")))
+	void *calloc(size_t nmemb, size_t size)
+	{
+		void *p = Malloc::Calloc(nmemb, size);
+		Monitor::Alloc(p, nmemb * size);
+		return p;
+	}
 
-    __attribute__ ((visibility ("default")))
-    void *realloc(void *ptr, size_t size)
-    {
-        void *p = Malloc::Realloc(ptr, size);
-        if (p)
-        {
-            Monitor::Free(ptr);
-            Monitor::Alloc(p, size);
-        }
-        else if (size == 0)
-        {
-            Monitor::Free(ptr);
-        }
-        return p;
-    }
+	__attribute__ ((visibility ("default")))
+	void *realloc(void *ptr, size_t size)
+	{
+		void *p = Malloc::Realloc(ptr, size);
+		if (p)
+		{
+			Monitor::Free(ptr);
+			Monitor::Alloc(p, size);
+		}
+		else if (size == 0)
+		{
+			Monitor::Free(ptr);
+		}
+		return p;
+	}
 
-    __attribute__ ((visibility ("default")))
-    int posix_memalign(void **memptr, size_t alignment, size_t size)
-    { return -1; }
+	__attribute__ ((visibility ("default")))
+	int posix_memalign(void **memptr, size_t alignment, size_t size)
+	{ return -1; }
 
-    __attribute__ ((visibility ("default")))
-    void *aligned_alloc(size_t alignment, size_t size)
-    { return NULL; }
+	__attribute__ ((visibility ("default")))
+	void *aligned_alloc(size_t alignment, size_t size)
+	{ return NULL; }
 
-    __attribute__ ((visibility ("default")))
-    void *valloc(size_t size)
-    { return NULL; }
+	__attribute__ ((visibility ("default")))
+	void *valloc(size_t size)
+	{ return NULL; }
 
-    __attribute__ ((visibility ("default")))
-    void *memalign(size_t alignment, size_t size)
-    { return NULL; }
+	__attribute__ ((visibility ("default")))
+	void *memalign(size_t alignment, size_t size)
+	{ return NULL; }
 
-    __attribute__ ((visibility ("default")))
-    void *pvalloc(size_t size)
-    { return NULL; }
+	__attribute__ ((visibility ("default")))
+	void *pvalloc(size_t size)
+	{ return NULL; }
 
 }
 
